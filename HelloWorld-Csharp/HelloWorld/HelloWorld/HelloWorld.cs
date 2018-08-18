771802e2a9ff388724c5858ad93002860646d6a0
Bob Henning <Bob@HenningEnterprises.local>
2018-08-18 00:15:04 -0400

    added HelloWorld - C#

---
+++ HelloWorld-Csharp/HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            SayHelloWorld(args);
+            Console.ReadLine();
+        }
+
+        public static int SayHelloWorld(String[] args)
+        {
+
+            int i = 1;
+            int Max = 3;
+
+            int firstArg = 0;
+
+            try
+            {
+                firstArg = (int)uint.Parse(args[0]);
+
+                Console.WriteLine("VALID: argument <" + args[0] + "> used");
+                Console.WriteLine("  ");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Argument <" + args[0] + "> must be a Positive Integer");
+                Console.WriteLine("  Using DEFAULT value: " + Max + " ");
+                Console.WriteLine("  ");
+
+                firstArg = Max;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Argument <" + args[0] + "> must be a Positive Integer");
+                Console.WriteLine("  Setting DEFAULT value: " + Max + " ");
+                Console.WriteLine("  ");
+
+                firstArg = Max;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("MISSING ARGUMENT: Argument must be a Positive Integer");
+                Console.WriteLine("  Using DEFAULT value: " + Max + " ");
+                Console.WriteLine("  ");
+
+                firstArg = Max;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR: argument <" + args[0] + "> caused an " + e + " error!");
+                Console.WriteLine("  Using DEFAULT value: " + Max + " ");
+                Console.WriteLine("  ");
+
+                firstArg = Max;
+            }
+            Max = firstArg;
+
+            while (i <= Max)
+            {
+                Console.WriteLine("Hello World ! " + i++ + " of " + Max);
+            }
+            Console.WriteLine(" ");
+
+            return --i;
+        }
+    }
+}
